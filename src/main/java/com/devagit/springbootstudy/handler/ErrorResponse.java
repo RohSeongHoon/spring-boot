e2c922e7ebaaa762e643b24659d1ac94230e2b19
Noh Seong Hoon <scubarsh@naver.com>
2022-01-31 17:05:30 +0900

    클라이언트쪽에 httpStatus의 value를 보내 서버쪽 잘못인지 클라이언트 잘못인지 확인할 수 있게 status.value를 추가해줌

--- src/main/java/com/devagit/springbootstudy/handler/ErrorResponse.java
+++ src/main/java/com/devagit/springbootstudy/handler/ErrorResponse.java
@@ -11,18 +11,22 @@ import org.springframework.http.ResponseEntity;
 public class ErrorResponse {
     private String message;
     private String error;
+    private int status;
 
     @Builder
-    private ErrorResponse(String error, String message) {
+    private ErrorResponse(String error, String message,int status) {
         this.error = error;
         this.message = message;
+        this.status = status;
     }
 
     public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
       return  ResponseEntity.status(errorCode.getHttpStatus())
                 .body(ErrorResponse.builder()
                         .error(errorCode.getHttpStatus().name())
-                        .message(errorCode.getDetailMessage()).build()
+                        .message(errorCode.getDetailMessage())
+                        .status(errorCode.getHttpStatus().value())
+                        .build()
                 );
     }
 
