6f9b7f70def5e6fc9d6481f6708c827b9e5ce2d7
Noh Seong Hoon <scubarsh@naver.com>
2022-02-10 16:36:28 +0900

    위치변경

--- src/main/java/com/devagit/springbootstudy/service/UserService.java
+++ src/main/java/com/devagit/springbootstudy/service/UserService.java
@@ -1,10 +1,10 @@
 package com.devagit.springbootstudy.service;
 
 import com.devagit.springbootstudy.domain.user.User;
-import com.devagit.springbootstudy.view.user.UserView;
+import com.devagit.springbootstudy.view.UserView;
 import com.devagit.springbootstudy.exceptionHandler.BusinessException;
 import com.devagit.springbootstudy.exceptionHandler.UserNotFoundException;
-import com.devagit.springbootstudy.repository.UserRepository;
+import com.devagit.springbootstudy.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
---
+++ src/main/java/com/devagit/springbootstudy/view/UserView.java
@@ -0,0 +1,24 @@
+package com.devagit.springbootstudy.view;
+
+import com.devagit.springbootstudy.domain.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserView {
+    private int id;
+    private String userName;
+    private String userId;
+
+    public UserView(int id, String userName, String userId) {
+        this.id = id;
+        this.userName = userName;
+        this.userId = userId;
+    }
+
+    public static UserView from(User user) {
+        return new UserView(user.getId(), user.getUsername(), user.getUserId());
+    }
+
+}
