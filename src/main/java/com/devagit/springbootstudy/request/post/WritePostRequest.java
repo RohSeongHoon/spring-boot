29ec11e1fa50ca84bcfafa287d1b84920485cf42
Noh Seong Hoon <scubarsh@naver.com>
2022-02-10 16:36:14 +0900

    글을 쓸때의 request생성

---
+++ src/main/java/com/devagit/springbootstudy/request/post/WritePostRequest.java
@@ -0,0 +1,18 @@
+package com.devagit.springbootstudy.request.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class WritePostRequest {
+    private int categoryId;
+    private int subCategoryId;
+    private String userId;
+    private String title;
+    private String contents;
+    private String writeDate;
+    private String source;
+}
