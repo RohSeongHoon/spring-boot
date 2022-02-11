17101fc8d0a175b3c088eb143c2f9bc5dd9c7932
Noh Seong Hoon <scubarsh@naver.com>
2022-02-11 15:44:43 +0900

    post전용 jpa생성 T는 Post ID는 integer타입

---
+++ src/main/java/com/devagit/springbootstudy/repository/post/PostJpaRepository.java
@@ -0,0 +1,8 @@
+package com.devagit.springbootstudy.repository.post;
+
+import com.devagit.springbootstudy.domain.posts.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostJpaRepository extends JpaRepository<Post,Integer> {
+
+}
