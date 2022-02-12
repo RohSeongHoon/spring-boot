0043c5c4629102fada55a81439793f8155ff392f
Noh Seong Hoon <scubarsh@naver.com>
2022-02-12 15:11:50 +0900

    postRepository에 게시글 아이디를 가지고 와서 보내주는 api생성

--- src/main/java/com/devagit/springbootstudy/repository/post/PostRepository.java
+++ src/main/java/com/devagit/springbootstudy/repository/post/PostRepository.java
@@ -1,9 +1,11 @@
 package com.devagit.springbootstudy.repository.post;
 
 import com.devagit.springbootstudy.domain.posts.Post;
+import com.devagit.springbootstudy.view.PostView;
 
 public interface PostRepository {
 
-    void save(Post post);
+    Post save(Post post);
 
+    Post findById(int id);
 }
