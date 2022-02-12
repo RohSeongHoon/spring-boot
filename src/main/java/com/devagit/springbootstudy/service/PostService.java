c23ffda6bd49ccc4411a064ca35275f70b9d5db9
Noh Seong Hoon <scubarsh@naver.com>
2022-02-12 15:12:35 +0900

    게시글을 작성 조회하는 api생성 (service)

--- src/main/java/com/devagit/springbootstudy/service/PostService.java
+++ src/main/java/com/devagit/springbootstudy/service/PostService.java
@@ -2,16 +2,38 @@ package com.devagit.springbootstudy.service;
 
 import com.devagit.springbootstudy.domain.posts.Post;
 import com.devagit.springbootstudy.repository.post.PostRepository;
+import com.devagit.springbootstudy.view.PostView;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
 
 
 @Service
 public class PostService {
-    private PostRepository postRepository;
-    public void writePost(int categoryId,int subCategoryId, String userId, String title, String contents, String source) {
-        String writDate = null;
-        Post post = new Post(categoryId,subCategoryId,userId,title,contents,writDate,source);
-        postRepository.save(post);
+    private final PostRepository postRepository;
 
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public void addPost(int categoryId, int subCategoryId, String userId, String title, String contents, String source) {
+        long time = System.currentTimeMillis();
+        SimpleDateFormat simpl = new SimpleDateFormat("yyyy:MM:dd hh:mm:ss");
+        String writeDate = simpl.format(time);
+        Post post = Post.builder()
+                .categoryId(categoryId)
+                .subCategoryId(subCategoryId)
+                .userId(userId)
+                .title(title)
+                .contents(contents)
+                .source(source)
+                .writeDate(writeDate)
+                .build();
+        postRepository.save(post);
+    }
+
+    public PostView getPost(int id) {
+        Post post = postRepository.findById(id);
+        return PostView.from(post);
     }
 }
