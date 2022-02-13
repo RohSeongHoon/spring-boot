10daf78bedc1d57143ecbeeab8922dc87cd1304d
Noh Seong Hoon <scubarsh@naver.com>
2022-02-13 17:36:27 +0900

    PostView에서 받아오는 매개변수가 많아서 Builder사용 필요없는 값은 null로 받고 필요한 요소들만 리턴을 할 수 있게 생성

---
+++ src/main/java/com/devagit/springbootstudy/view/post/PostView.java
@@ -0,0 +1,52 @@
+package com.devagit.springbootstudy.view.post;
+
+import com.devagit.springbootstudy.domain.posts.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Getter
+@NoArgsConstructor
+public class PostView {
+    private int id;
+    private int subCategoryId;
+    private String userId;
+    private String title;
+    private String contents;
+    private String source;
+    private String writeDate;
+
+    @Builder
+    public PostView(int id, int subCategoryId, String userId, String title, String contents, String source, String writeDate) {
+        this.id = id;
+        this.subCategoryId = subCategoryId;
+        this.userId = userId;
+        this.title = title;
+        this.contents = contents;
+        this.source = source;
+        this.writeDate = writeDate;
+    }
+    public static PostView from(Post post) {
+        return PostView.builder()
+                .id(post.getId())
+                .subCategoryId(post.getSubCategoryId())
+                .userId(post.getUserId())
+                .title(post.getTitle())
+                .contents(post.getContents())
+                .source(post.getSource())
+                .writeDate(post.getWriteDate())
+                .build();
+    }
+    public static PostView postList(Post post){
+        return PostView.builder()
+                .id(post.getId())
+                .subCategoryId(post.getSubCategoryId())
+                .userId(post.getUserId())
+                .title(post.getTitle())
+                .writeDate(post.getWriteDate())
+                .build();
+    }
+}
