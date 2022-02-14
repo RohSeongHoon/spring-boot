f2d37c9365bcf1771824426c1a2af332a718b0f9
Noh Seong Hoon <scubarsh@naver.com>
2022-02-14 18:17:32 +0900

    UpdateRequest생성

---
+++ src/main/java/com/devagit/springbootstudy/request/post/UpdateRequest.java
@@ -0,0 +1,14 @@
+package com.devagit.springbootstudy.request.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//이 클래스는 deleteRequest클래스와 요청이 같은데 하나로 묶어도 되는지 질문
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UpdateRequest {
+    private int id;
+
+}
