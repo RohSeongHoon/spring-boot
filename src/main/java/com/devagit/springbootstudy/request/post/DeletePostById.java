471c7adabe0a18661534bb12f5134042c8ad816f
Noh Seong Hoon <scubarsh@naver.com>
2022-02-14 18:17:12 +0900

    deleteRequest생성 값은 id만 받아도 상관없음

---
+++ src/main/java/com/devagit/springbootstudy/request/post/DeletePostById.java
@@ -0,0 +1,13 @@
+package com.devagit.springbootstudy.request.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class DeletePostById {
+    private int id;
+
+}
