f7efdf0cb81c07e4a28e0eeb03667d960f3a8b3e
Noh Seong Hoon <scubarsh@naver.com>
2022-02-15 17:47:57 +0900

    예외 클래스를 생성할때 하나하나 클래스를 생성하지않고 NotFoundException클래스로 정의를 해놓고 사용하면 더욱 좋은 코드가 된다

--- src/main/java/com/devagit/springbootstudy/exceptionHandler/PostNotFoundException.java
+++ src/main/java/com/devagit/springbootstudy/exceptionHandler/PostNotFoundException.java
@@ -6,4 +6,6 @@ public class PostNotFoundException extends BusinessException {
     public PostNotFoundException(ErrorCode errorCode) {
         super(errorCode);
     }
+    //404 클래스를 만들고 상속해서 사용 Not Found 예외로 하는것으로사용
+    //
 }
