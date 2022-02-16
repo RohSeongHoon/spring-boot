01c9943c0c867e416964dbfeb0d28e693be2ccc4
Noh Seong Hoon <scubarsh@naver.com>
2022-02-16 17:00:03 +0900

    @AllargsConstructor에 오류가있어서 사용하지 않는다

--- src/main/java/com/devagit/springbootstudy/request/post/UpdateRequest.java
+++ src/main/java/com/devagit/springbootstudy/request/post/UpdateRequest.java
@@ -1,15 +1,29 @@
 package com.devagit.springbootstudy.request.post;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-//이 클래스는 deleteRequest클래스와 요청이 같은데 하나로 묶어도 되는지 질문
-@AllArgsConstructor
+import java.util.Date;
+
+
 @NoArgsConstructor
 @Getter
 public class UpdateRequest {
     private int id;
+    private int subCategoryId;
+    private String userId;
+    private String title;
+    private String contents;
+    private String source;
+    private Date createAt;
 
+    public UpdateRequest(int id, int subCategoryId, String userId, String title, String contents, String source, Date createAt) {
+        this.id = id;
+        this.subCategoryId = subCategoryId;
+        this.userId = userId;
+        this.title = title;
+        this.contents = contents;
+        this.source = source;
+        this.createAt = createAt;
+    }
 }
-//수정된 시간
