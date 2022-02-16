c9941635847a4fd728535c7bea3b5e30cac3976b
Noh Seong Hoon <scubarsh@naver.com>
2022-02-16 17:01:25 +0900

    writeAt 을 관례인 createAt으로 변경 String타입에서 Date타입으로 변경  수정한 날짜를 저장하는 updateAt도 추가

--- src/main/java/com/devagit/springbootstudy/domain/post/Post.java
+++ src/main/java/com/devagit/springbootstudy/domain/post/Post.java
@@ -1,8 +1,10 @@
 package com.devagit.springbootstudy.domain.post;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -28,20 +30,24 @@ public class Post {
     //무조건 있어야하는 것은 DB에서 넣는게 맞다
     //넣는 방법은 더 생각
     //createdAt 을 관용 , 업데이트: updatedAt, deletedAt 관용암
+    @CreationTimestamp
+    @Column(columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP",nullable = false)
+    private Date createAt;
+    @CreationTimestamp
     @Column(columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP",nullable = true)
-    private String writeDate;
+    private Date updateAt;
     @Column(columnDefinition = "VARCHAR(64)")
     private String source;
 
     @Builder
-    public Post(int categoryId, int subCategoryId, String userId, String title, String contents, String source,String writeDate) {
+    public Post(int categoryId, int subCategoryId, String userId, String title, String contents, String source) {
         this.categoryId = categoryId;
         this.subCategoryId = subCategoryId;
         this.userId = userId;
         this.title = title;
         this.contents = contents;
         this.source = source;
-        this.writeDate = writeDate;
+
     }
 }
 
