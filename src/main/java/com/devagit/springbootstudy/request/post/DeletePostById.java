767a809938bdd1ec9571daccace7b3b4f1694f65
Noh Seong Hoon <scubarsh@naver.com>
2022-02-16 17:05:16 +0900

    게시물을 삭제할 때 id값만 받아오는것이 아닌 user의 아이디도 받아서 검증후 삭제 하도록 변경

--- src/main/java/com/devagit/springbootstudy/request/post/DeletePostById.java
+++ src/main/java/com/devagit/springbootstudy/request/post/DeletePostById.java
@@ -8,9 +8,12 @@ import lombok.NoArgsConstructor;
 @Getter
 public class DeletePostById {
     private int id;
+    private String userId;
 
-    public DeletePostById(int id) {
+
+    public DeletePostById(int id, String userId) {
         this.id = id;
+        this.userId = userId;
     }
 }
 //게시글 주인만 지울 수 있게 아이디도 같이 받아와야한다
