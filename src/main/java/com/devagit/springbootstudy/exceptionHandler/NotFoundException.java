9b43cddfcc41b34eb190c5574c75b4cc36063350
Noh Seong Hoon <scubarsh@naver.com>
2022-02-17 23:11:50 +0900

    BusinessException을 상속받은 NotFoundException 생성 NotFount는 다 여기서 관리를 한다 메세지만 받아서 전송해준다

---
+++ src/main/java/com/devagit/springbootstudy/exceptionHandler/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.devagit.springbootstudy.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends BusinessException {
+
+    public NotFoundException(String message) {
+        super(new ErrorCode(HttpStatus.NOT_FOUND, message));
+    }
+}
