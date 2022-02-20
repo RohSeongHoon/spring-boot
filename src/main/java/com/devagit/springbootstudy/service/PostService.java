1696069faae014b8a178013fda0c92406e7e9c9f
Noh Seong Hoon <scubarsh@naver.com>
2022-02-20 21:26:46 +0900

    예외처리 방식 변경에 맞게 수정및 게시물 요청시 몇개씩 보여줄지 기본값 설정 게시물 리스트를 처음 요청할 때 커서 위치를 0으로 설정

--- src/main/java/com/devagit/springbootstudy/service/PostService.java
+++ src/main/java/com/devagit/springbootstudy/service/PostService.java
@@ -1,11 +1,14 @@
 package com.devagit.springbootstudy.service;
 
 import com.devagit.springbootstudy.domain.post.Post;
-import com.devagit.springbootstudy.exceptionHandler.PostNotFoundException;
-import com.devagit.springbootstudy.exceptionHandler.UserNotFoundException;
+import com.devagit.springbootstudy.exceptionHandler.NotFoundException;
 import com.devagit.springbootstudy.repository.post.PostRepository;
 import com.devagit.springbootstudy.view.post.PostListView;
 import com.devagit.springbootstudy.view.post.PostView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -14,13 +17,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static com.devagit.springbootstudy.exceptionHandler.ErrorCode.POST_NOT_FOUND;
-import static com.devagit.springbootstudy.exceptionHandler.ErrorCode.USER_ID_NOT_THE_SAME;
-
 
 @Service
 public class PostService {
     private final PostRepository postRepository;
+    private static final int DEFAULT_SIZE = 5; //컨트롤러에서? 서비스에서?
 
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
@@ -41,16 +42,18 @@ public class PostService {
     }
 
     public PostView getPost(int id) {
-        Post post = Optional.ofNullable(postRepository.findById(id)).orElseThrow(() -> new PostNotFoundException(POST_NOT_FOUND));
+        Post post = Optional.ofNullable(postRepository.findById(id)).orElseThrow(() -> new NotFoundException("게시글이 존재하지 않습니다"));
         return PostView.from(post);
         //예외처리 꼭하기 명심
     }
 
-    public List<PostListView> getPostList(int subCategoryId) {
-        return postRepository.findBySubCategoryId(subCategoryId)
+    public List<PostListView> getPostList(int subCategoryId,Integer cursorId,Integer size) {
+        if (size == null){size = DEFAULT_SIZE;}
+        if (cursorId==null){cursorId = 0;}
+        Pageable page = PageRequest.of(cursorId,size);
+        return postRepository.findBySubCategoryIdOrderByIdDesc(subCategoryId,page)
                 .stream()
-                .filter(post -> post.getSubCategoryId() == subCategoryId)
-                .sorted(Comparator.comparing(Post::getCreateAt))
+                .sorted(Comparator.comparing(Post::getCreatedAt))
                 .map(PostListView::from)
                 .limit(20)          //db에서
                 .collect(Collectors.toList());
@@ -59,14 +62,14 @@ public class PostService {
     public List<PostListView> findPostsByUserId(String userId) {
         List<PostListView> posts = postRepository.findByUserId(userId)
                 .stream()
-                .sorted(Comparator.comparing(Post::getCreateAt))
+                .sorted(Comparator.comparing(Post::getCreatedAt))
                 .map(PostListView::from)
                 .collect(Collectors.toList());
         return posts; //db에서 정렬해서 가지고오기
     }
 
     public void deletePostById(int id, String userId) {
-        Post post = Optional.ofNullable(postRepository.findById(id)).orElseThrow(() -> new PostNotFoundException(POST_NOT_FOUND));
+        Post post = Optional.ofNullable(postRepository.findById(id)).orElseThrow(() -> new NotFoundException("삭제할 아이디가 존재하지 않습니다"));
         if (userId.equals(post.getUserId())) {
             postRepository.deletePostById(id);
         }
@@ -75,13 +78,13 @@ public class PostService {
     public int updatePost(int id, int subCategoryId, String userId, String title, String contents, String source, Date createAt) {
         Post post = postRepository.findById(id);
         if (!userId.equals(post.getUserId())) {
-            throw new UserNotFoundException(USER_ID_NOT_THE_SAME);
+            throw new NotFoundException("아이디가 일치하지 않습니다");
         }
         post.setSubCategoryId(subCategoryId);
         post.setTitle(title);
         post.setContents(contents);
         post.setSource(source);
-        post.setUpdateAt(createAt);
+        post.setUpdatedAt(createAt);
         postRepository.save(post); //새로들어온걸 from으로 넣어서 save로 하면댄다 이럴때 이해 실패
         return post.getId();
 
