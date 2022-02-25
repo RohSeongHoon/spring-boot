a94f5b7f8ff1ac4e09d7a6cab45ab8ed72ed5de7
Noh Seong Hoon <scubarsh@naver.com>
2022-02-25 13:22:19 +0900

    댓글 sevice를 생성

--- src/main/java/com/devagit/springbootstudy/service/CommentService.java
+++ src/main/java/com/devagit/springbootstudy/service/CommentService.java
@@ -2,9 +2,18 @@ package com.devagit.springbootstudy.service;
 
 import com.devagit.springbootstudy.domain.comment.Comment;
 import com.devagit.springbootstudy.repository.comment.CommentRepository;
+import com.devagit.springbootstudy.util.MakePageAble;
 import com.devagit.springbootstudy.view.comment.CommentView;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.Order;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -16,10 +25,31 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    public CommentView addComment(int postId, String userId, String content) {
-        Comment comment = new Comment(postId,userId,content);
+    public CommentView addComment(int parentId,int postId, String userId, String content,int sorts) {
+        Comment comment = Comment.builder()
+                .parentId(parentId)
+                .postId(postId)
+                .userId(userId)
+                .content(content)
+                .sorts(sorts)
+                .build();
         return CommentView.from(commentRepository.save(comment));
     }
 
 
+    public List<CommentView> getCommentsList(int postId, Date commentCursor, int page,int size) {
+        if (size == 0) {
+            size = MakePageAble.DEFAULT_SIZE;
+        }
+        if (commentCursor  == null){
+            Date now = new Date();
+            commentCursor = now;
+        }
+       Pageable pageable = PageRequest.of(page, size, Sort.Direction.DESC,"sorts","parentId","createdAt");
+        return commentRepository.findAllByPostId(postId,pageable)
+                .stream()
+                .map(CommentView::from)
+                .collect(Collectors.toList());
+
+    }
 }
