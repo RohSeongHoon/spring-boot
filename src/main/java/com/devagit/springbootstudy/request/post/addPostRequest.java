f1317f313277bc25289466d9630170fa5438d31e
Noh Seong Hoon <scubarsh@naver.com>
2022-02-26 16:59:43 +0900

    writePost에서 addPost로 클래스명 변경

---
+++ src/main/java/com/devagit/springbootstudy/request/post/addPostRequest.java
@@ -0,0 +1,26 @@
+package com.devagit.springbootstudy.request.post;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Getter
+public class addPostRequest {
+    private int categoryId;
+    private int subCategoryId;
+    private String userId;
+    private String title;
+    private String contents;
+    private String source;
+
+
+    public addPostRequest(int categoryId, int subCategoryId, String userId, String title, String contents, String source) {
+        this.categoryId = categoryId;
+        this.subCategoryId = subCategoryId;
+        this.userId = userId;
+        this.title = title;
+        this.contents = contents;
+        this.source = source;
+    }
+}
