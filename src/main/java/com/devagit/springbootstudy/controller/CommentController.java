569a6d3f43e33e0278fd7880986c9aeb007d9400
Noh Seong Hoon <scubarsh@naver.com>
2022-02-26 16:55:38 +0900

    댓글 수정과 삭제 추가

--- src/main/java/com/devagit/springbootstudy/controller/CommentController.java
+++ src/main/java/com/devagit/springbootstudy/controller/CommentController.java
@@ -1,6 +1,8 @@
 package com.devagit.springbootstudy.controller;
 
 import com.devagit.springbootstudy.request.comment.CommentRequest;
+import com.devagit.springbootstudy.request.comment.DeleteCommentRequest;
+import com.devagit.springbootstudy.request.comment.UpdateCommentRequest;
 import com.devagit.springbootstudy.service.CommentService;
 import com.devagit.springbootstudy.view.comment.CommentView;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +19,21 @@ public class CommentController {
     }
 
     @PostMapping("api/v1/comments")
-    public CommentView addComment(@RequestBody CommentRequest req){
-        return commentService.addComment(req.getParentId(),req.getPostId(), req.getUserId(), req.getContent(),req.getSorts());
+    public CommentView addComment(@RequestBody CommentRequest req) {
+        return commentService.addComment(req.getParentId(), req.getPostId(), req.getUserId(), req.getContent(), req.getSorts());
     }
+
     @GetMapping("api/v1/comments/list")
-    public List<CommentView> getCommentsList(@RequestParam int postId, Date commentCursor, int page, int size){
-        return commentService.getCommentsList(postId,commentCursor,page,size);
+    public List<CommentView> getCommentsList(@RequestParam int postId, Date commentCursor, int page, int size) {
+        return commentService.getCommentsList(postId, commentCursor, page, size);
     }
 
-
+    @DeleteMapping("api/v1/comments")
+    public long deleteComment(@RequestBody DeleteCommentRequest req) {
+        return commentService.deleteComment(req.getId(),req.getUserId());
+    }
+    @PutMapping("api/v1/comments")
+    public CommentView updateComments(@RequestParam UpdateCommentRequest req){
+        return commentService.updateComments(req.getId(),req.getUserId(),req.getContent(),req.getCreatedAt());
+    }
 }
