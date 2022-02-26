5ef5489a708897917ef110a4123262f127809a5b
Noh Seong Hoon <scubarsh@naver.com>
2022-02-26 16:58:27 +0900

    서비스에 수정,삭제 기능추가 수정은 createdAt을 받아서 db에 createdAt에 삽입해주고 updateAt은 현재의 시간을 default로 설정해놔서 넣지않음 삭제는 하위댓글이있다면 하위댓글의 갯수를 리턴하도록 되어있고 댓글이 없다면 삭제하고 0을리턴하도록 만듦

--- src/main/java/com/devagit/springbootstudy/service/CommentService.java
+++ src/main/java/com/devagit/springbootstudy/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.devagit.springbootstudy.service;
 
 import com.devagit.springbootstudy.domain.comment.Comment;
+import com.devagit.springbootstudy.exceptionHandler.NotFoundException;
 import com.devagit.springbootstudy.repository.comment.CommentRepository;
 import com.devagit.springbootstudy.util.MakePageAble;
 import com.devagit.springbootstudy.view.comment.CommentView;
@@ -10,9 +11,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.Order;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -25,7 +28,7 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    public CommentView addComment(int parentId,int postId, String userId, String content,int sorts) {
+    public CommentView addComment(int parentId, int postId, String userId, String content, int sorts) {
         Comment comment = Comment.builder()
                 .parentId(parentId)
                 .postId(postId)
@@ -37,19 +40,43 @@ public class CommentService {
     }
 
 
-    public List<CommentView> getCommentsList(int postId, Date commentCursor, int page,int size) {
+    public List<CommentView> getCommentsList(int postId, Date commentCursor, int page, int size) {
         if (size == 0) {
             size = MakePageAble.DEFAULT_SIZE;
         }
-        if (commentCursor  == null){
+        if (commentCursor == null) {
             Date now = new Date();
             commentCursor = now;
         }
-       Pageable pageable = PageRequest.of(page, size, Sort.Direction.DESC,"sorts","parentId","createdAt");
-        return commentRepository.findAllByPostId(postId,pageable)
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.DESC, "sorts", "parentId", "createdAt");
+        return commentRepository.findAllByPostId(postId, pageable)
                 .stream()
                 .map(CommentView::from)
                 .collect(Collectors.toList());
 
+    }
+
+    public long deleteComment(int id, String userId) {
+        Comment comment = Optional.ofNullable(commentRepository.findById(id)).orElseThrow(() -> new NotFoundException("없는 정보입니다"));
+        if (!userId.equals(comment.getUserId())) {
+            throw new NotFoundException("정보가 일치하지 않습니다");
+        }
+        long count = commentRepository.countByParentId(id);
+        if (count != 0) {
+            return count;
+        }
+        commentRepository.deleteCommentsById(id);
+        return 0;
+    }
+
+    public CommentView updateComments(int id, String userId, String content, Timestamp createdAt) {
+        Comment comment = Optional.ofNullable(commentRepository.findById(id)).orElseThrow(() -> new NotFoundException("없는 정보입니다"));
+        if (!userId.equals(comment.getUserId())) {
+            throw new NotFoundException("정보가 일치하지 않습니다");
+        }
+        comment.setContent(content);
+        comment.setCreatedAt(createdAt);
+        commentRepository.save(comment);
+        return CommentView.from(comment);
     }
 }
