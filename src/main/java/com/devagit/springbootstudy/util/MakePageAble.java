37d03013d3e93deee8240666b6025feda25271d8
Noh Seong Hoon <scubarsh@naver.com>
2022-02-27 18:10:28 +0900

    pageable을 만들어주는 util클래스 수정 static으로 현재시간을 만들어주는 currentTime을 생성해줌

--- src/main/java/com/devagit/springbootstudy/util/MakePageAble.java
+++ src/main/java/com/devagit/springbootstudy/util/MakePageAble.java
@@ -3,19 +3,16 @@ package com.devagit.springbootstudy.util;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Date;
+import java.sql.Timestamp;
 
 public class MakePageAble {
     public static final int DEFAULT_SIZE = 5;
+    public static Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 
 
-    public static Pageable makePageAble(Date cursor, int size, int page) {
-        if (size == 0) {
+    public static Pageable makePageAble( int page, Integer size) {
+        if (size == null) {
             size = DEFAULT_SIZE;
-        }
-        if (cursor == null) {
-            Date now = new Date();
-            cursor = now;
         }
         return PageRequest.of(page, size);
     }
