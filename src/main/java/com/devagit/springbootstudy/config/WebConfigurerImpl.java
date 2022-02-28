d7833dc91290e871b77e5fa10790bffd8045aac9
Noh Seong Hoon <scubarsh@naver.com>
2022-02-28 19:12:21 +0900

    cors로 인해 설정파일 추가

--- src/main/java/com/devagit/springbootstudy/config/WebConfigurerImpl.java
+++ src/main/java/com/devagit/springbootstudy/config/WebConfigurerImpl.java
@@ -1,8 +1,10 @@
 package com.devagit.springbootstudy.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfigurerImpl implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
