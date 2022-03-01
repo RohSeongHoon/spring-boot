5ec740357be36c0f5092458c5de70e879e6a1974
Noh Seong Hoon <scubarsh@naver.com>
2022-03-01 19:14:30 +0900

    분리한거에 맞게 서비스추가 post의 heart개수를 하나올리거나 내리거나 하게만드는 로직 추가

--- src/main/java/com/devagit/springbootstudy/service/HeartService.java
+++ src/main/java/com/devagit/springbootstudy/service/HeartService.java
@@ -1,29 +1,38 @@
 package com.devagit.springbootstudy.service;
 
 import com.devagit.springbootstudy.domain.Heart;
+import com.devagit.springbootstudy.exceptionHandler.notfound.HeartNotFoundException;
 import com.devagit.springbootstudy.repository.heart.HeartRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 //count대신 컬럼을 생성
 @Service
 public class HeartService {
     private HeartRepository heartRepository;
+    private PostService postService;
 
-    public HeartService(HeartRepository heartRepository) {
+    public HeartService(HeartRepository heartRepository, PostService postService) {
         this.heartRepository = heartRepository;
+        this.postService = postService; //service끼리? 아니면 repository?
     }
 
     //두개로 나눠야함
-    public boolean addOrDeleteHeart(int postId, String userId) {
-        if (!findHeartByUserId(postId, userId)) {
-            deleteHeart(postId, userId);
-            return false;
-        }
-        Heart heart = new Heart(postId, userId);
-        heartRepository.save(heart);
+    @Transactional
+    public boolean addHeart(int postId, String userId) {
+        heartRepository.save(new Heart(postId, userId));
+        postService.setHeartCnt(postId, 1);
         return true;
+    }
+
+    @Transactional
+    public boolean deleteHeart(int postId, String userId) {
+        Optional.ofNullable(heartRepository.findByPostIdAndUserId(postId, userId)).orElseThrow(() -> new HeartNotFoundException("못찾음"));
+        heartRepository.deleteByPostIdAndUserId(postId, userId);
+        postService.setHeartCnt(postId, -1);
+        return false;
     }
 
     public Boolean findHeartByUserId(int postId, String userId) {
@@ -34,13 +43,6 @@ public class HeartService {
         return false;
     }
 
-    @Transactional
-    public void deleteHeart(int postId, String userId) {
-        heartRepository.deleteByPostIdAndUserId(postId, userId);
-    }
 
-    @Transactional
-    public int getHeartCount(int postId) {
-        return heartRepository.countByPostId(postId);
-    }
+
 }
