5e5b3267afe5487d5be0ccbff88253ba56ee3fbd
Noh Seong Hoon <scubarsh@naver.com>
2022-03-01 19:11:45 +0900

    heart를 제거할때 이미 삭제되었을 때에 대한 예외처리

---
+++ src/main/java/com/devagit/springbootstudy/exceptionHandler/notfound/HeartNotFoundException.java
@@ -0,0 +1,7 @@
+package com.devagit.springbootstudy.exceptionHandler.notfound;
+
+public class HeartNotFoundException extends NotFoundException{
+    public HeartNotFoundException(String message) {
+        super(message);
+    }
+}
