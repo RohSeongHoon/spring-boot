f0d4c9c9de1b04825b040b3b6a20a1ea4ed15686
Noh Seong Hoon <scubarsh@naver.com>
2022-03-16 18:09:31 +0900

    성별로 받는것을 updatedAt을 이용해서 정렬해서 가지고 오게 변경

--- src/main/java/com/devagit/springbootstudy/repository/user/UserJpaRepository.java
+++ src/main/java/com/devagit/springbootstudy/repository/user/UserJpaRepository.java
@@ -1,18 +1,24 @@
 package com.devagit.springbootstudy.repository.user;
 
 import com.devagit.springbootstudy.domain.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 
 public interface UserJpaRepository extends JpaRepository<User, String> {
     User findByUsername(String username);
 
-    User findByUserId(String userId);
+    Optional<User> findByUserId(String userId);
 
     @Transactional
     void deleteByUserId(String userId);
 
     User findByEmail(String email);
+
+    List<User> findByGenderAndUpdatedAtLessThanEqualOrderByUpdatedAtAsc(String gender, LocalDateTime updatedAt, Pageable pageable);
 }
