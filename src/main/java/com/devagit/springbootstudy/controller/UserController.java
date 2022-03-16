2a69ffd3b521350b77db90e2e1dc73efa436fe1e
Noh Seong Hoon <scubarsh@naver.com>
2022-03-16 17:59:38 +0900

    유저의 프로필을 보는데 성별로 나눈 api생성

--- src/main/java/com/devagit/springbootstudy/controller/UserController.java
+++ src/main/java/com/devagit/springbootstudy/controller/UserController.java
@@ -4,11 +4,12 @@ package com.devagit.springbootstudy.controller;
 import com.devagit.springbootstudy.request.user.*;
 import com.devagit.springbootstudy.service.UserService;
 import com.devagit.springbootstudy.util.Page;
+import com.devagit.springbootstudy.view.user.DetailProfileView;
 import com.devagit.springbootstudy.view.user.UserProfileView;
 import com.devagit.springbootstudy.view.user.UserView;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8080")
@@ -20,20 +21,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    //로그인 기능
-    //아이디 중복확인
-    //post로 아이디와 비번을 보냄
-    //db에서 기본키인 아이디를 검색
-    //아이디가 있으면 비밀번호 비교
-    //비밀번호가 같다면 로그인 성공
-    //비밀번호가 틀리다면 로그인 실패
-    //아이디가 없다면 예외처리
+   //검색필터를 사용할때는 queryDSL을 사용하는것이 맞는지
 
 
     //회원 가입  ===================================
     @PostMapping(value = "/api/v1/users")
     public UserView signUp(@RequestBody UserRequest req) {
-        return userService.signUp(req.getUserId(),req.getBirthday(), req.getUsername(), req.getPassword(), req.getEmail(), req.getProfileImg(), req.getIntroduction(), req.getInstarId());
+        return userService.signUp(req.getUserId(), req.getBirthday(), req.getUsername(), req.getPassword(), req.getGender(), req.getEmail(), req.getProfileImg(), req.getIntroduction(), req.getInstarId());
     }
 
     //로그인 ===================================
@@ -43,7 +37,7 @@ public class UserController {
     }
 
     //회원 정보 조회 ===================================
-    @PostMapping("api/v1/users/find-password") //대쉬
+    @PostMapping("/api/v1/users/find-password") //대쉬
     public String findPasswordByUserId(@RequestBody FindPasswordByUserIdRequest req) {
         return userService.findPasswordByUserId(req.getUserId(), req.getUsername(), req.getEmail());
     }
@@ -62,23 +56,30 @@ public class UserController {
     public List<UserView> findUserByEmailDomain(@RequestParam(required = false) String emailDomain) {
         return userService.findUserByEmailDomain(emailDomain);
     }
-    @GetMapping("api/v1/user/profile")
-    public Page<UserProfileView> findUserByGender(@RequestParam(required = false) String gender,Integer page,Integer size ){
-        return userService.findUserByGender(gender,page,size);
+
+    @GetMapping("/api/v1/profile")
+    public Page<UserProfileView> findUserByGender(@RequestParam(required = false) String gender, LocalDateTime updatedAt, Integer page, Integer size) {
+        return userService.findUserByGender(gender, updatedAt, page, size);
     }
+
+    @GetMapping("/api/v1/{user}/profile")
+    public DetailProfileView getDetailProfile(@PathVariable("userId") String userId) {
+        return userService.findByUserId(userId);
+    }
+
     //회원 정보 변경 ===================================
-    @PutMapping("api/v1/users/change-userPassword")
+    @PutMapping("/api/v1/users/change-userPassword")
     public void changeId(@RequestBody ChangeUserPasswordRequest req) {
         userService.changeUserPassword(req.getUserId(), req.getPassword(), req.getNewPassword());
     }
 
-    @PutMapping("api/v1/users/chang-personalInfo")
+    @PutMapping("/api/v1/users/chang-personalInfo")
     public void changePersonalInfo(@RequestBody ChangeUserInfoRequest req) {
-        userService.changePersonalInfo(req.getUserId(), req.getPassword(), req.getUsername(), req.getEmail(),req.getUpdatedAt());
+        userService.changePersonalInfo(req.getUserId(), req.getPassword(), req.getUsername(), req.getEmail(), req.getUpdatedAt());
     }
 
     //회원 정보 삭제 ===================================
-    @DeleteMapping("api/v1/users")
+    @DeleteMapping("/api/v1/users")
     public void deleteUser(@RequestBody DeleteUserRequest req) {
         userService.deleteUser(req.getUserId(), req.getPassword());
     }
