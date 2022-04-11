8c8b087c3dd6af8e46339a3573d4b6eb8697a984
RohSeongHoon <scubarsh@naver.com>
2022-04-12 00:24:09 +0900

    외부 통신 api테스트 코드 수정

--- src/test/java/com/devagit/springbootstudy/infra/NaverMapClientTest.java
+++ src/test/java/com/devagit/springbootstudy/infra/NaverMapClientTest.java
@@ -1,14 +1,17 @@
 package com.devagit.springbootstudy.infra;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class NaverMapClientTest {
+    @Autowired
     NaverMapClient naverMapClient;
 
-    public void getNaverMapTest(){
-        naverMapClient.getNaverMap();
+    @Test
+    public void getNaverMapTest() {
+        System.out.println(naverMapClient.getNaverMap());
     }
 }
