056e3754ce80ffb1318691827bd815431156f7d5
RohSeongHoon <scubarsh@naver.com>
2022-04-13 22:59:39 +0900

    카테고리를 가지올 json에 맞게 생성한 class

---
+++ src/main/java/com/devagit/springbootstudy/response/CategoriesResponseEntity.java
@@ -0,0 +1,25 @@
+package com.devagit.springbootstudy.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoriesResponseEntity {
+    private int id;
+    private String name;
+    private String nameKr;
+
+    public CategoriesResponseEntity(int id, String name, String nameKr) {
+        this.id = id;
+        this.name = name;
+        this.nameKr = nameKr;
+    }
+
+
+
+}
