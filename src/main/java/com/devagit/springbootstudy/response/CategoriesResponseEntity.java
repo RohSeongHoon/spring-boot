a9ed3731ec19515b787db3ef99a2cd3a5cd23a97
RohSeongHoon <scubarsh@naver.com>
2022-04-15 23:34:13 +0900

    entity는 헤더정보가지 포함한객체이기 때문에 네이밍 수정을 해야함

--- src/main/java/com/devagit/springbootstudy/response/CategoriesResponseEntity.java
+++ src/main/java/com/devagit/springbootstudy/response/CategoriesResponseEntity.java
@@ -12,14 +12,12 @@ import lombok.Setter;
 public class CategoriesResponseEntity {
     private int id;
     private String name;
+    @JsonProperty("name_kr")
     private String nameKr;
 
-    public CategoriesResponseEntity(int id, String name, String nameKr) {
+    public CategoriesResponseEntity(int id, String name, String nameKr) { //
         this.id = id;
         this.name = name;
         this.nameKr = nameKr;
     }
-
-
-
 }
