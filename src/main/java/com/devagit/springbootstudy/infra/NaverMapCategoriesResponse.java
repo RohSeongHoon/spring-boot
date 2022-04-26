28762b24400fc51abe5d6c792ca17b15b2aa9b22
RohSeongHoon <scubarsh@naver.com>
2022-04-26 23:38:06 +0900

    response객체 생성

--- src/main/java/com/devagit/springbootstudy/infra/NaverMapCategoriesResponse.java
+++ src/main/java/com/devagit/springbootstudy/infra/NaverMapCategoriesResponse.java
@@ -1,4 +1,21 @@
 package com.devagit.springbootstudy.infra;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class NaverMapCategoriesResponse {
+    private int id;
+    private String name;
+    private int parentNum;
+
+    public NaverMapCategoriesResponse(int id, String name,@Nullable int parentNum) {
+        this.id = id;
+        this.name = name;
+        this.parentNum = parentNum;
+    }
 }
