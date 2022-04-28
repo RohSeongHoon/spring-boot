a636487f1aa1b29dc9337dcf6901d62a7f5a8ee6
RohSeongHoon <scubarsh@naver.com>
2022-04-28 20:00:10 +0900

    getDetailcompanyInfo 테스트 코드 추가

--- src/test/java/com/devagit/springbootstudy/infra/MovieCompanyClientTest.java
+++ src/test/java/com/devagit/springbootstudy/infra/MovieCompanyClientTest.java
@@ -1,21 +1,20 @@
 package com.devagit.springbootstudy.infra;
 
-import com.devagit.springbootstudy.response.MovieCompanyResponse;
+import com.devagit.springbootstudy.response.DetailCompanyInfoResponse.CompanyInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MovieCompanyClientTest {
     @Autowired
     private MovieCompanyClient movieCompanyClient;
-
     @Test
-    public void getMovieCompanyListTest(){
-        MovieCompanyResponse.CompanyListResult result = movieCompanyClient.getMovieCompanyList(null,null);
-        System.out.println(result.getTotCnt());
+    public void detailCompanyInfo(){
+        CompanyInfo result = movieCompanyClient.getDetailCompanyInfo("20122497");
+        System.out.println(result.getCompanyNm());
     }
 
 }
