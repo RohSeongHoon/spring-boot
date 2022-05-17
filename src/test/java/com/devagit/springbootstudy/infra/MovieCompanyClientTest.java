c9f9c3d246e4c3b65340838edba0428195916f38
RohSeongHoon <scubarsh@naver.com>
2022-05-17 19:17:59 +0900

    테스트 코드 디테일추가

--- src/test/java/com/devagit/springbootstudy/infra/MovieCompanyClientTest.java
+++ src/test/java/com/devagit/springbootstudy/infra/MovieCompanyClientTest.java
@@ -7,14 +7,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class MovieCompanyClientTest {
     @Autowired
     private MovieCompanyClient movieCompanyClient;
+
     @Test
-    public void detailCompanyInfo(){
+    public void detailCompanyInfo() {
         CompanyInfo result = movieCompanyClient.getDetailCompanyInfo("20122497");
         System.out.println(result.getCompanyNm());
+        System.out.println(result.getFilmos().get(0).getMovieCd());
     }
-
 }
