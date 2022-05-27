677c5e180ae09eca9f1dba8609cf4c0fc61872f3
RohSeongHoon <scubarsh@naver.com>
2022-05-27 12:48:44 +0900

    dynamicUpdate,dynamicInsert 테스트 코드 추가

--- src/test/java/com/devagit/springbootstudy/service/schedule/MovieCompanyScheduleTest.java
+++ src/test/java/com/devagit/springbootstudy/service/schedule/MovieCompanyScheduleTest.java
@@ -1,5 +1,6 @@
 package com.devagit.springbootstudy.service.schedule;
 
+import com.devagit.springbootstudy.domain.MovieCompany;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +13,23 @@ class MovieCompanyScheduleTest {
     private MovieCompanySchedule movieCompanySchedule;
 
     @Test
-    public void MovieCompanyScheduleTest(){
+    public void MovieCompanyScheduleTest() {
         movieCompanySchedule.saveMovieClient();
+    }
+
+    @Test
+    public void DynamicInsertTest() {
+        MovieCompany movieCompany = new MovieCompany("key123", "회사명", "어떤곳123");
+        movieCompanySchedule.testDynamicInsert(movieCompany);
+    }
+
+    @Test
+    public void DynamicUpdateTest() {
+        long start = System.currentTimeMillis();
+        MovieCompany movieCompany = new MovieCompany("key", "다시12", "어떤곳");
+        movieCompanySchedule.testDynamicUpdate(movieCompany);
+        long end = System.currentTimeMillis();
+        System.out.println((end-start)/1000.0);
     }
 
 }
