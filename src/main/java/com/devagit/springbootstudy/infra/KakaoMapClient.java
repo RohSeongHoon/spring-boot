99c83733dcc0aaddaa2cf8e199e55522173dd518
RohSeongHoon <scubarsh@naver.com>
2022-05-31 16:57:15 +0900

    infra에 카카오 맵 openApi를 사용하는 cilent추가

---
+++ src/main/java/com/devagit/springbootstudy/infra/KakaoMapClient.java
@@ -0,0 +1,40 @@
+package com.devagit.springbootstudy.infra;
+
+import com.devagit.springbootstudy.response.AddressResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Component
+public class KakaoMapClient {
+    private RestTemplate restTemplate;
+
+    public KakaoMapClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getAddress(String keyWord) {
+        URI uri = UriComponentsBuilder
+                .newInstance()
+                .scheme("https")
+                .host("dapi.kakao.com")
+                .path("/v2/local/search/keyword.json")
+                .queryParam("query", keyWord)
+                .encode()
+                .build()
+                .toUri();
+
+        RequestEntity requestEntity = RequestEntity
+                .get(uri)
+                .header("Authorization","KakaoAK " + "0e057614f227f19627d37df78b497c1f")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(requestEntity, String.class);
+        return result.getBody();
+    }
+}
